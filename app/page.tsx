e67abd91a7ba089393ccaf5f14a29e2cf5854e40
Philip Rutberg <54883264+rutbergphilip@users.noreply.github.com>
2026-03-09 20:58:40 +0100

    responsivitet

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { GameBoard } from './components/GameBoard';
 export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-gray-50'>
-      <main className='max-w-xl rounded-2xl bg-white p-10 shadow-lg text-center flex flex-col items-start gap-4'>
+      <main className='w-full max-w-2xl rounded-2xl bg-white p-4 sm:p-10 shadow-lg text-center flex flex-col items-start gap-4'>
         <GameBoard />
       </main>
     </div>
